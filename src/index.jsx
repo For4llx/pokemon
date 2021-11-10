11312852fc3cc5783de19b689a4dd1d9dd1d653d
Nobime <romeo.nobime@mailo.com>
2021-11-10 15:40:50 +0100

    Ajout du responsive et des animations

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Card from "./components/Card";
-// import "./css/style.css";
+import "./css/style.css";
 
 ReactDOM.render(
   <React.StrictMode>
+    <Card />
+    <Card />
     <Card />
   </React.StrictMode>,
   document.getElementById("root")
